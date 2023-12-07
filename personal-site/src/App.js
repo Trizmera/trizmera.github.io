375397a6b4902477db1c43600b1807b1c5e11571
Beatriz Cruz <mllebia@live.com>
2023-12-06 22:00:51 -0300

    first version

---
+++ personal-site/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav class="navbar background">
+        <ul class="nav-list">
+          <div class="logo">
+            <h3>Beatriz Cruz</h3>
+          </div>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div class="rightNav">
+
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
